0dd509bc80dd980fe586cebc1d62be6186a85813
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-03-10 21:54:39 +0200

    Little fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs" />
       <HomeCards />
       <JobListings />
       <ViewAllJobs />

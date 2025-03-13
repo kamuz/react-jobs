6fa7d28e9181b3fd9117cb1a1d3b15f3e23a779b
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-03-13 18:16:49 +0200

    Added NotFoundPage and JobsPage

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
+
+const JobPage = () => {
+  return (
+    <>
+      <JobListings />
+    </>
+  );
+}
+
+export default JobPage;

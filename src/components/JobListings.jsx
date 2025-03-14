3837afe22704517957b17713ff8630f7ed6ad3c0
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-03-14 21:42:01 +0200

    Reverse job listings

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -3,7 +3,9 @@ import JobListing from './JobListing';
 
 const JobListings = ({isHome = false}) => {
 
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  // Create a shallow copy of the jobs array and reverse it
+  const latestJobsFirst = [...jobs].reverse(); // or jobs.slice().reverse()
+  const jobListings = isHome ? latestJobsFirst.slice(0, 3) : latestJobsFirst;
 
   return (
     <section className="bg-blue-50 px-4 py-10">

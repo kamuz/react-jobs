5b62bbb2e34384375ce4d67a5dc1b8bb7595ac34
Volodymyr Kamuz <v.kamuz@gmail.com>
2025-03-15 21:02:12 +0200

    Replace with ternary operator

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -39,10 +39,12 @@ const JobListings = ({isHome = false}) => {
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">{isHome ? 'Recent Jobs' : 'Browse Jobs'}</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {loading && <p>Loading jobs...</p>}
-          {jobs.map((job) => (
-            <JobListing key={ job.id } job={ job }/>
-          )) }
+          {
+            loading ? <p>Loading jobs...</p> :
+            jobs.map((job) => (
+              <JobListing key={ job.id } job={ job }/>
+            ))
+          }
         </div>
       </div>
     </section>
